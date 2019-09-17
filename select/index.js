2dd475617d8e28454dcb9abc2ab99b00c641fc21
tarif hossain <tarifhossainhmd@gmail.com>
2019-09-18 01:45:54 +0600

    this is the angularJs file upload commit

---
+++ select/index.js
@@ -0,0 +1,17 @@
+var app = angular.module("myApp",[]);
+app.controller("myCtrl",function($scope){
+$scope.item=[
+    "raihan",
+    "shafik",
+    "rafan",
+    "hasan",
+    "shan"
+];
+$scope.items=[
+    {Name:"raihan",Email:".hjskhf@"},
+    {Name:"shafik",Email:"hkhf199@"},
+    {Name:"rafan",Email:"hjskgfhfdgsahf@"},
+    {Name:"hasan",Email:"hjs457dskhf@"},
+    {Name:"shan",Email:"hjsrt87khf@"}
+]
+});
---
+++ table/index.js
@@ -0,0 +1,11 @@
+var app = angular.module("myApp",[]);
+app.controller("myCtrl",function($scope){
+$scope.items=[
+    {Name:"raihan",Email:"t.hjskhf@"},
+    {Name:"shafik",Email:"hkhf199@"},
+    {Name:"rafan",Email:"hjskgfhfdgsahf@"},
+    {Name:"hasan",Email:"hjs457dskhf@"},
+    {Name:"shan",Email:"hjsrt87khf@"}
+]
+
+});
